b643cb7cd7844276fa3ad75f27901d5653660584
loveleetjoshi <loveleetjoshi@gmail.com>
2023-09-03 16:46:13 +0530

    episode-3/part-3 (Done)

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", { id: "heading" }, "Namastey React!");
 console.log(heading);
 
+// JSX :- is not HTML-in-JS but HTML/XML like syntax
+// JSX cannot be understand by browsers
+// JSX is transpilled before it reaches to JS Engine => Parcel => Babel
+
+// JSX => Babel transpiles in to React.createEleemnt => object => HTMLElement(render)
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Namastey React using JSX
+  </h1>
+);
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
