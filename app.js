5e098df94e41eee2adbeeef4323c68e89cdc7e4a
loveleetjoshi <loveleetjoshi@gmail.com>
2023-09-03 16:17:29 +0530

    episode-3/part-2 (Done)

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-/**
- * <div id='parent'>
- *      <div id='child'>
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *      </div>
- * </div>
- */
+// React Element
+// React.createEleemnt => object => HTMLElement(render)
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {id: 'heading'}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ])
-);
-console.log(parent);
+const heading = React.createElement("h1", { id: "heading" }, "Namastey React!");
+console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
 
-root.render(parent); // ReactElement(Object) => HTML(Browser understands)
+root.render(heading);
